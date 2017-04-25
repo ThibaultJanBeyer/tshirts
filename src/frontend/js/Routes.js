f993f917440108f65b44e7e67f2937aba3a8e8b3
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2017-04-25 09:06:04 +0200

    fix 404 route with switch. fix subscribeemail css.

--- src/frontend/js/Routes.js
+++ src/frontend/js/Routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import { HomePage, ShopPage, NotFound } from "components";
 
 const Routes = () => {
   return (
-    <div>
+    <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
       <Route component={NotFound} />
-    </div>
+    </Switch>
   );
 };
 
