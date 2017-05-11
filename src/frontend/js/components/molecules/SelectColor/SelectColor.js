e166c94461977220d5e11430315331550ecff620
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2017-05-11 23:02:28 +0200

    add state to color & select size

--- src/frontend/js/components/molecules/SelectColor/SelectColor.js
+++ src/frontend/js/components/molecules/SelectColor/SelectColor.js
@@ -1,45 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { queryHelper } from 'components';
 import help from "components/themes/helpers.css";
 
 import style from './SelectColor.css';
 
-const SelectColor = ({ colors, checked }) => {
-  const handleChange = (ev) => {
-    const value = ev.target.value;
-    // @TODO add store state changes
-  };
+class SelectColor extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const Colors = colors.map(color => (
-    <label  key={color.key}
-            className={
-              `${
-                style["SelectColor__element"]
-                } ${
-                (color.key === checked) ? style["SelectColor__element--checked"] : ""
-              }`
-            }
-            style={{ background: color.hex }} >
+    // @TODO add store state from backend
+    this.state = {
+      color: this.props.checked
+    };
 
-      <input  type="radio"
-              className={help["visuallyhidden"]}
-              name="color"
-              onChange={handleChange}
-              value={color.key} />
-      <span className={help["visuallyhidden"]}>{color.key}</span>
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    </label>
-  ));
+  handleChange(ev) {
+    // @TODO should also change the url query
+    const color = ev.target.value;
+    this.setState({
+      color
+    });
+  }
 
-  return (
-    <fieldset role="group" aria-labelledby="color">
-      <legend id="color">Color</legend>
-      {Colors}
-    </fieldset>
-  );
-};
+  render() {
+    const Colors = this.props.colors.map(color => {
+      const checked = color.key === this.state.color;
+      const dark = color.hex < "#777";
+
+      return (
+        <label  key={color.key}
+                className={
+                  `${
+                    style["SelectColor__element"]
+                    } ${
+                    (checked) ? style["SelectColor__element--checked"] : ""
+                  }`
+                }
+                style={{ background: color.hex,
+                         color: (dark) ? "#fff" : "#000" }} >
+
+          <input  type="radio"
+                  className={help["visuallyhidden"]}
+                  name="color"
+                  onChange={this.handleChange}
+                  value={color.key} />
+          <span className={help["visuallyhidden"]}>{color.key}</span>
+
+        </label>
+      );
+    }, this);
+
+    return (
+      <fieldset role="group" aria-labelledby="color">
+        <legend id="color">Color</legend>
+        {Colors}
+      </fieldset>
+    );
+  }
+}
 
 SelectColor.defaultProps = {
   colors: [
@@ -62,13 +83,21 @@ SelectColor.defaultProps = {
     {
       key: "blue",
       hex: "#00f"
+    },
+    {
+      key: "teal",
+      hex: "#0ff"
+    },
+    {
+      key: "yellow",
+      hex: "#ff0"
     }
   ]
 };
 
 SelectColor.propTypes = {
   colors: PropTypes.arrayOf(PropTypes.object),
-  color: PropTypes.string  // selected color
+  checked: PropTypes.string  // selected color
 };
 
 export default SelectColor;
--- src/frontend/js/components/molecules/SelectSize/SelectSize.js
+++ src/frontend/js/components/molecules/SelectSize/SelectSize.js
@@ -2,35 +2,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // import { theme } from 'components';
+import help from "components/themes/helpers.css";
 
-// import style from './SelectSize.css';
+import style from './SelectSize.css';
 
-const SelectSize = ({ property }) => {
+const SelectSize = ({ sizes, checked }) => {
+  const Sizes = sizes.map(size => {
+    const checked = size === checked;
+    return (
+      <label  key={size}
+              className={
+                `${
+                  style["SelectSize__element"]
+                  } ${
+                  (checked) ? style["SelectSize__element--checked"] : ""
+                }`
+              } >
+
+        <input  type="radio"
+                className={help["visuallyhidden"]}
+                name="size"
+                value={size} />
+        <span>{size}</span>
+
+      </label>
+    );
+  }, this);
+
   return (
     <fieldset role="group" aria-labelledby="size">
       <legend id="size">Size</legend>
-      <label>
-        <input  type="radio"
-                name="size"
-                value="black" />
-        small
-      </label>
-      <label>
-        <input  type="radio"
-                name="size"
-                value="black" />
-        medium
-      </label>
+      {Sizes}
     </fieldset>
   );
 };
 
 SelectSize.defaultProps = {
-  property: 'something'
+  sizes: [
+    'xs',
+    's',
+    'm',
+    'l',
+    'xl',
+    'xxl',
+  ],
+  checked: 'm'
 };
 
 SelectSize.propTypes = {
-  property: PropTypes.string
+  sizes: PropTypes.arrayOf(PropTypes.string),
+  checked: PropTypes.string  // selected color
 };
 
 export default SelectSize;
