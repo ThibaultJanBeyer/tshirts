0036492847e10d329259f9cdcb763b7b10ccd906
Thibault Beyer <tbeyer@MB00904.local>
2017-05-12 15:21:55 +0200

    improve ItemSettings style

--- src/frontend/js/components/organisms/ItemSettings/ItemSettings.js
+++ src/frontend/js/components/organisms/ItemSettings/ItemSettings.js
@@ -4,47 +4,57 @@ import { withRouter } from 'react-router-dom';
 import queryString from 'query-string';
 
 import { 
+  Button,
   FlexibleImg,
   ImagesPresentation,
+  ItemPrice,
   ReactionBar,
   SelectColor,
   SelectPosition,
-  SelectSize
+  SelectSize,
+  SelectType
 } from 'components';
 
 import style from './ItemSettings.css';
 
 const ItemSettings = ({ item, location }) => {
   const artistsSelectedDefaultColor = "black";  // @TODO: replace this with store values
-  const { color=artistsSelectedDefaultColor } = queryString.parse(location.search);
+  const artistsSelectedDefaultPosition = "front";  // @TODO: replace this with store values
+  const artistsSelectedDefaultType = "Unisex T-Shirt";  // @TODO: replace this with store values
+  const { color=artistsSelectedDefaultColor,
+          size="m",
+          position=artistsSelectedDefaultPosition,
+          type=artistsSelectedDefaultType } = queryString.parse(location.search);
 
-  console.log(color);
   return (
     <form className={style['ItemSettings']}>
-      {/* title */}
-      <h1>{item}</h1>
 
-      {/* Reaction bar */}
-      <ReactionBar />
-
-      {/* selections */}
-      <SelectColor checked={color} />
-      <SelectSize />
-      <SelectPosition />
-
-      {/* type selection */}
-      <button>Change Producs Type</button>
-
-      {/* price */}
-      <div>
-        15€
-        Includes VAT
+      <div className={style['ItemSettings__container']}>
+        <h1>{item}</h1>
+        <ReactionBar />
       </div>
 
-      {/* add to cart */}
-      <input type="submit" value="add to cart" />
+      <div className={style['ItemSettings__container']}>
+        <SelectType checked={type} />
+      </div>
+      <div className={style['ItemSettings__container']}>
+        <SelectColor checked={color} />
+      </div>
+      <div className={style['ItemSettings__container']}>
+        <SelectSize checked={size} />
+      </div>
+      <div className={style['ItemSettings__container']}>
+        <SelectPosition checked={position} />
+      </div>
 
-      {/*additiona info*/}
+      <div className={style['ItemSettings__container']}>
+        <ItemPrice color={color} _type={type}/>
+      </div>
+
+      <div className={`${style['ItemSettings__container']} ${style['ItemSettings__submit']}`}>
+        <Button content="Add to cart" primary />
+      </div>
+
       <div>
         <p>
           Shipping to Germany available
