290a190c4ea62e6b96243dc6ab6459111f5d0938
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2017-05-17 09:07:07 +0200

    start extracting to imagewithtitle component

---
+++ src/frontend/js/components/molecules/ImageWithTitle/ImageWithTitle.js
@@ -0,0 +1,84 @@
+/*import React from "react";
+import PropTypes from "prop-types";
+import { Link } from 'react-router-dom';
+
+import style from "./ImageWithTitle.css";
+import { 
+  FlexibleImg,
+  theme
+} from "components";
+
+// see http://stackoverflow.com/questions/31079081/programmatically-navigate-using-react-router
+const ImageWithTitle = ({ 
+  linkUrl,
+  imgUrl,
+  imgAlt,
+  title,
+  children
+}) => {
+
+  let heading;
+  if(level === 1) {
+    heading = (
+      <h1 className={style["ImageWithTitle__title"]}>
+        {title}
+      </h1>
+    );
+  } else if(level === 2) {
+    heading = (
+      <h2 className={style["ImageWithTitle__title"]}>
+          {title}
+      </h2>
+    );
+  } else if(level === 3) {
+    heading = (
+      <h3 className={style["ImageWithTitle__title"]}>
+          {title}
+      </h3>
+    );
+  } else if(level === 4) {
+    heading = (
+      <h4 className={style["ImageWithTitle__title"]}>
+          {title}
+      </h4>
+    );
+  }
+
+  return (
+    <div  className={style["ImageWithTitle"]}>
+
+      <div className={style["ImageWithTitle__head"]}>
+        <Link to={linkUrl}>
+          <FlexibleImg source={imgUrl} altText={imgAlt}/>
+        </Link>
+      </div>
+
+      <div className={style["ImageWithTitle__body"]}>
+        <Link to={linkUrl} className={theme.link}>
+          {heading}
+        </Link>
+        <div>
+          {children}
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+GalleryItem.defaultProps = {
+  linkUrl,
+  imgUrl,
+  imgAlt,
+  title
+};
+
+GalleryItem.propTypes = {
+  linkUrl,
+  imgUrl,
+  imgAlt,
+  title,
+  children
+};
+
+export default GalleryItem;*/
